6d78aa066dff62935ac3864ee42056eb4d9e8aef
mikedesousa92 <mikedesousa92@gmail.com>
2025-04-10 12:10:55 +0000

    :tada: fim da aula 01

---
+++ lib/ui/_core/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color backgroundColor = Color(0xFF202123);
+  static const Color mainColor = Color(0xFFFFA559);
+  static const Color textColor = Colors.white;
+}
